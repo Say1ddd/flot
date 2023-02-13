91607195f9140a8b4f371d64bb769b0ad56f2dea
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-02-13 19:35:53 +0700

    Add: Admin Auth Middleware

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Users/Index', [
+            'users' => User::all(),
+        ]);
+    }
+}
---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->user()->role !== 'admin') {
+        
+            session()->flush();
+            return redirect()->route('dashboard')->with('error', 'Not found.');
+        }
+        
+        return $next($request);
+    }
+}
