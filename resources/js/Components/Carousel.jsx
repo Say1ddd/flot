c68666a66029af3c487a92968ad7f08e1e6c160f
Say1ddd <sajidmuhammadikhlas@gmail.com>
2024-02-02 19:47:49 +0700

    Add: carousel

--- resources/js/Components/Carousel.jsx
+++ resources/js/Components/Carousel.jsx
@@ -3,11 +3,10 @@ import '../../css/components/carousel.css';
 
 const Carousel = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
-    // const slides = ['Slide 1', 'Slide 2', 'Slide 3'];
     const slides = [
-        'https://source.unsplash.com/random/800x600',
-        'https://source.unsplash.com/random/800x601',
-        'https://source.unsplash.com/random/800x602'
+        'https://source.unsplash.com/random/1600x900',
+        'https://source.unsplash.com/random/1600x901',
+        'https://source.unsplash.com/random/1600x902'
     ];
 
     useEffect(() => {
@@ -15,12 +14,20 @@ const Carousel = () => {
             setCurrentSlide((prevSlide) => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1));
         }, 3000);
 
+        
         return () => clearInterval(interval);
     }, []);
 
     return (
         <div className="carousel">
-            <div className="slide">{slides[currentSlide]}</div>
+            {slides.map((slide, index) => (
+                <div
+                    key={index}
+                    className={`slide ${index === currentSlide ? 'active' : ''}`}
+                >
+                    <img src={slide} alt="Slide" />
+                </div>
+            ))}
             <div className="indicator">
                 {slides.map((slide, index) => (
                     <div
